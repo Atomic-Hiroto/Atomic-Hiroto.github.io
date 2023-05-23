a03e3d17fdad3f9e8710fb639d18280f2734ba4a
Pritam Borole <borolepritam@gmail.com>
2023-05-23 17:46:00 +0530

    Navbar and Hero

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import {Element} from "react-scroll"
+import headshot from "../images/h1.jpg"
 
 function Home(props) {
     return (
-        <div id='home'>
-            <Element name='home'>
-                home
+            <Element id='home' name='home'>
+                <div>
+                    <img className="home-img" src={headshot} />
+                </div>
+                <div id="user-detail-intro">
+                    <p>Hi, I'm PRI<span className='acc'>TAM</span></p>
+                </div>
             </Element>
-        </div>
     );
 }
 
